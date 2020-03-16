d2ae8408b5a9c49fbe1b07e34e7e4cb0f2e21c51
alanbanks229 <alanbanks229@gmail.com>
2020-03-16 11:17:54 -0400

    help

--- js/index.js
+++ js/index.js
@@ -22,12 +22,16 @@ function renderBook(book) {
     let bookId = book.id
 
     console.log(bookId)
+    let readBtn = document.createElement('button')
+    readBtn.innerText = "Read Book"
+    readBtn.addEventListener('click', () => addReader(book))
     let div = document.createElement('div')
     let showPanel = document.getElementById('show-panel')
     let image = document.createElement('img')
     let description = document.createElement('p')
     let title = document.createElement('h2')
     let ul = document.createElement('ul')
+    debugger
     book.users.forEach( user => ul.append(displayUser(user)) )
     
 
@@ -35,7 +39,7 @@ function renderBook(book) {
     description.innerText = book.description
     title.innerText = book.title
 
-    div.append(title, image, description, ul)
+    div.append(title, image, description, ul, readBtn)
     showPanel.innerHTML = ""
 
     showPanel.append(div)
@@ -45,4 +49,25 @@ function displayUser(user){
     let li = document.createElement('li')
     li.innerText = user.username
     return li
+}
+
+function addReader(book_object){
+    console.log(book_object)
+    debugger
+    let currentUsers = book_object.users
+    let user =   {
+        "id": 1,
+        "username": "pouros"
+      }
+    currentUsers.push(user)
+    fetch(`http://localhost:3000/books/${book_object.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            Accept : "application/json"
+        },
+        body: JSON.stringify({"users" : currentUsers })
+    })
+    .then(response => response.json())
+    .then( (book_object) => console.log(book_object))
 }
